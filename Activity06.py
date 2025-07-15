82208478cea30ffd32d3faa38bedca8e39fab929
German Escobar <germanjosearielescobarescobar@gmail.com>
2025-07-15 14:35:42 -0600

    Commit de Actividad 6

--- Activity06.py
+++ Activity06.py
@@ -15,5 +15,29 @@ for i in range(cantidad):
         "preciouni":preciouni,
         "stock":stock,
     }
+a = 0
+print("\nLista de Productos:")
+for codigo,datos in productos.items():
+    print("\n")
+    print(f"\nProducto No.{a+1}")
+    print(f"\nCódigo del Producto: {codigo}")
+    print(f"\nNombre: {datos['nombre']}")
+    print(f"\nCategoria: {datos['categoria']}")
+    print(f"\nTalla: {datos['talla']}")
+    print(f"\nPrecio Unitario: {datos['preciouni']}")
+    print(f"\nStock: {datos['stock']}")
+    print("\n")
+
+print("\nBuscar Producto")
+buscar=input("\nIngrese Código del Producto: ")
+if buscar in productos:
+    print(f"\nNombre: {productos[buscar]['nombre']}")
+    print(f"\nCategoria: {productos[buscar]['categoria']}")
+    print(f"\nTalla: {productos[buscar]['talla']}")
+    print(f"\nPrecio Unitario: {productos[buscar]['preciouni']}")
+    print(f"\nStock: {productos[buscar]['stock']}")
+
+else:
+    print("Producto no Encontrado")
 
 
